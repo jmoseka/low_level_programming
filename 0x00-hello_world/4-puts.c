b68a661333170b9079bd58ddd954a35d64354e39
jmoseka <jamylam3@gmail.com>
2021-08-26 09:38:28 -0700

    a c program that prints a string using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
 #!/bin/bash
-gcc char Mystr[] = "\"\"Programming is like building a multilingual puzzle" puts(Mystr)
+int main()
+{
+  printf("\"\"Programming is like building a multilingual puzzle");
+  return 0;
+}
