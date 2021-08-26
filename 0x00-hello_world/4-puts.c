2669b8e3e3b94e3090e3b0242bcb5236aa15e341
jmoseka <jamylam3@gmail.com>
2021-08-26 10:48:09 -0700

    a C program that prints using fuction puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-int main(void)
+
+int main()
 {
-  puts=("\"Programming is like building a multilingual puzzle");
- return 0;
+    puts("\"Programming is like building a multilingual puzzle");
+   
+    return 0;
 }
