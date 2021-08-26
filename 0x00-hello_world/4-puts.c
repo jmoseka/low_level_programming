6373844f0b33d3bacd5c9d844c0e4b10cefd059e
jmoseka <jamylam3@gmail.com>
2021-08-26 11:45:58 -0700

    a program that prints with function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-/*
-*A function that prints with function puts
+/**
+* main - program prints with function puts
+*
+*Return: Always 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");   
-return {0};
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
