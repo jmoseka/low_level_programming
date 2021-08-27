a6b9fb16f9e89bf56c45465933fc8759e2fe5331
jmoseka <jamylam3@gmail.com>
2021-08-27 11:19:00 -0700

    program that prints positive or negative from a random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(n>0){
+        printf("positive");
+	}
+	else if(n==0)
+        {
+        printf("zero");
+        }
+        else{
+        printf("negative");
+        }
+	return (0);
+}
