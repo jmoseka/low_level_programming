15ad7ea9f9014ae02dfa1355b49155214301341c
jmoseka <jamylam3@gmail.com>
2021-08-28 03:09:38 -0700

    a program that prints alphabets except q and e

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints alphabets except q and e
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return 0;
+}
