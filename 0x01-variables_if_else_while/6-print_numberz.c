843fcc6c052ce652a4c3894d387fe48c205a2477
jmoseka <jamylam3@gmail.com>
2021-08-28 05:06:19 -0700

    print number using char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-	char ch = '0';
+	int ch;
 
-	for (ch = '0'; ch <= '9'; ch++)
+	for (ch = 0; ch < 10; ch++)
 	{
-		putchar(ch);
+		putchar(ch + '0');
 	}
 	putchar('\n');
 	return (0);
