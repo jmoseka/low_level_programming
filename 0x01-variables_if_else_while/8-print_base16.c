3f2f1d503953479aeedd2312770484ba7c02a563
jmoseka <jamylam3@gmail.com>
2021-08-28 05:56:35 -0700

    printing both letter and number using putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - a program that print letters and no.s using putchar
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+}
