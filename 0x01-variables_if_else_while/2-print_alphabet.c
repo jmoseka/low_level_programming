5e4a37eacc25ef910a327f5decd49c9d4c4f2ca8
jmoseka <jamylam3@gmail.com>
2021-08-28 02:50:42 -0700

    a program that prints alphabets using putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints alphabets
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
