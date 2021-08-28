0c49c83f7afd7a1bbfa0d721693d24e23ec3d6b6
jmoseka <jamylam3@gmail.com>
2021-08-27 20:33:41 -0700

    last digit that has evaluation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,16 +19,16 @@ int main(void)
 	/* your code goes there */
 	lastDigit = n % 10;
 	if (lastDigit > 5)
-    {
+       	{
         printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-    }
-    else if(lastDigit == 0)
-    {
+	}
+       	else if(lastDigit == 0)
+       	{
         printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-    }
-    else
-    {
+       	}
+       	else
+       	{
         printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-    }
+       	}
 	return (0);
 }
