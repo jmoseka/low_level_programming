e1291171aa04cf7553e7bc29f577da39a7d929e3
jmoseka <jamylam3@gmail.com>
2021-08-28 03:49:45 -0700

    print numbers using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main -  a program that prints all single digit numbers of base ten using putchar
+ * main -  a program that prints numbers using putchar
  *
  * Return: return 0
  */
