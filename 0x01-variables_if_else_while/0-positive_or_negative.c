ca8167f68e3a4f2182cf3c70e002b3c2aceb3d4a
jmoseka <jamylam3@gmail.com>
2021-08-30 07:00:47 -0700

    program that evaluates a random number whether positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,23 +12,22 @@
 int main(void)
 {
 	int n;
-	int lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	lastDigit = n % 10;
 	if (lastDigit > 5)
-       	{
-        printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	{
+		printf("%d is positive\n", n);
 	}
-       	else if (lastDigit == 0)
-       	{
-        printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-       	}
-       	else
-       	{
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-       	}
+	else if (lastDigit == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
