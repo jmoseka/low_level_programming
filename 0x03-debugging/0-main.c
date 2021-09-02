545a364355e8d303217f59ce3c3a2521ec593db0
jmoseka <jamylam3@gmail.com>
2021-09-02 01:15:53 -0700

    test postitive_or_negative function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * main - check the code for Holberton School students.
  *
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-void positive_or_negative(int i);
+
+#include <stdio.h>
+
+void positive_or_negative(int i)i;
 #endif
