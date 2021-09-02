a91a0157f16f50b7e6ab91a3776dd09dfafec0c5
jmoseka <jamylam3@gmail.com>
2021-09-02 02:12:18 -0700

    debugging an infitite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,13 +10,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
+	/*
 	i = 0;
 
 	while (i < 10)
 	{
 		putchar(i);
 	}
+	*/
 
 	printf(" Infinite loop avoided  ! \\0/\n");
 
