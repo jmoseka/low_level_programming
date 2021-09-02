abc558df47a4021087a3dc2a3cabe06871f5cd12
jmoseka <jamylam3@gmail.com>
2021-09-02 10:06:19 -0700

    a function that prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_square - func that prints 10 times the numbers, from 0 to 14
+ *
+ * @size:  integer return type
+ *
+ */
+
+void print_square(int size)
+{
+	int i = 0, j;
+
+	if (size < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < size)
+		{
+			j = 0;
+
+			while (j <= size)
+			{
+				_putchar('#');
+
+				j++;
+			}
+			_putchar('\n');
+
+			i++;
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,5 @@ void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_square(int size);
 #endif
