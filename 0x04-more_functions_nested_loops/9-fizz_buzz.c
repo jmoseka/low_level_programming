7d17703850a78160549131aa71068d77b7547827
jmoseka <jamylam3@gmail.com>
2021-09-03 00:29:51 -0700

    fizz buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,20 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
-		printf(" ");
 		i++;
 	}
 	return (0);
