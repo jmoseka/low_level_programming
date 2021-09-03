05110348497bf0a5c996ed9390d9d3c4853541b9
jmoseka <61447440+jmoseka@users.noreply.github.com>
2021-09-03 17:25:58 +0300

    update printing triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,9 +16,9 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 1; i <= size; i++)
+		for (i = rows; i >= 1; --)
 		{
-			for (j = 1; j <= i; j++)
+			for (j = i; j >= 1; j--)
 			{
 				_putchar('#');
 			}
