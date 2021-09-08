deeb9a6fa44b4362c91faba4d8fe51545ef01606
jmoseka <jamylam3@gmail.com>
2021-09-08 05:00:35 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-string_toupper.c -o 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* *string_toupper -  changes all lowercase letters of a string to uppercase
+* @s: source string.
+* Return: an uppercase string
+*/
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+	s[i] = toupper(s[i]);
+
+		i++;
+	}
+	return (s);
+}
