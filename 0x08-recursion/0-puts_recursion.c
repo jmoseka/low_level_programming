1a5f8993924056f49765c11e543f6730db2d6fc8
jmoseka <jamylam3@gmail.com>
2021-09-15 07:05:51 -0700

    a function that prints a string puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_puts_recursion - checks for a character in a string
+*@s: A pointer to be printed
+*/
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
