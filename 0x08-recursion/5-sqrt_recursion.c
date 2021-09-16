fb24eb8187ef670b9d1340c67a0c21575e586a11
jmoseka <jamylam3@gmail.com>
2021-09-16 11:09:24 -0700

    a function that returns the natural square root of a number.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+  
+#include "main.h"
+/**
+ * _sqrt_recursion -  returns the natural square root of a number.
+ * @n: integer
+ * Return: squareroot or -1
+ */
+int _sqrt_recursion(int n)
+{
+if (n == 0)
+return (0);
+return (check(1, n));
+}
+/**
+ * check - checks for the square root
+ * @a: integer
+ * @b: integer
+ * Return: int
+ */
+int check(int a, int b)
+{
+if (a * a > b)
+return (-1);
+else if (a * a == b)
+return (a);
+else
+return (check(a + 1, b));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,4 +8,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int is_prime_number(int n);
 int isPrime(int i, int n);
+int _sqrt_recursion(int n);
+int check(int i, int n);
 #endif
