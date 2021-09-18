f20876227ee841fe6877d623c4fef5e25fa85033
jmoseka <jamylam3@gmail.com>
2021-09-18 12:33:34 -0700

    a static library libmy.a

---
+++ 0x09-static_libraries/_isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _alpha - function that checks for alphabetic character
+ *
+ * @c: return char type
+ *
+ * Return: return 0 on success and 1 on fail
+ */
+
+int _alpha(int c)
+{
+	return (0);
+}
