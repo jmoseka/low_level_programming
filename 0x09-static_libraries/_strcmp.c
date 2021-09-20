58ba8aa2344614271bd91da7236c25c6c026c0c3
jmoseka <jamylam3@gmail.com>
2021-09-20 00:46:57 -0700

    added function _strcmp.c to static library libmy.a

---
+++ 0x09-static_libraries/_strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp - compares two strings.
+* @s1: array one
+* @s2: array two
+* Return: result
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+
+	return (result);
+}
