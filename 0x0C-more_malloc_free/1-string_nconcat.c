707ded4a25dc2e170a45d32f5bc18a0745f8d4fb
jmoseka <jamylam3@gmail.com>
2021-09-24 17:20:08 +0000

    a function that concatenates two strings and stores in a mallocked acquired string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,11 +2,12 @@
 /**
  * string_nconcat - concatenates two strings
  * @s1: destination string
- * @S2: n bytes of s2
- * n: bytes
+ * @s2: n bytes of the string pointer
+ * @n: bytes
  *
  * Return: a mallocked pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ch;
@@ -27,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ch[i] = s1[i];
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; s2[j] && j < n; j++)
 	{
 		ch[i] = s2[j];
 		i++;
