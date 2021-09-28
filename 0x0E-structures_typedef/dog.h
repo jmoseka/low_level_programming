49fe8e7b6c23a607f266b6810cf549e1954c3942
jmoseka <jamylam3@gmail.com>
2021-09-28 18:14:32 +0000

    a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,11 @@
-#include <stddef.h>
 #ifndef DOG_H
 #define DOG_H
 
 /**
- *struct my_struct - Short description
+ *struct dog - containing members of dog
  *@name: member name
- *@b: member age
- *@c: member owner
+ *@age: member age
+ *@owner: member owner
  *
  *Description: define data for struct dog
  */
