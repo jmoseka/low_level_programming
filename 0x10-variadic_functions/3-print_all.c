fdf7848acad6d5800779b5f5802fa98b75a307e0
jmoseka <jamylam3@gmail.com>
2021-10-05 18:47:05 +0000

    prints all using variadic functions and checking specifiers

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,77 @@
+#include "variadic_functions.h"
+#include<stdio.h>
+#include<stdarg.h>
+/**
+ *  * print_char - prints char
+ *   * @valist: valist
+ *    */
+void print_char(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+/**
+ *  * print_int - prints int
+ *   * @valist: valist
+ *    */
+void print_int(va_list valist)
+{
+	printf("%d", va_arg(valist, int));
+}
+/**
+ *  * print_float - prints float
+ *   * @valist: valist
+ *    */
+void print_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+/**
+ *  * print_string - prints string
+ *   * @valist: valist
+ *    */
+void print_string(va_list valist)
+{
+	char *s;
+	s = va_arg(valist, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
+
+/**
+ *  * print_all - print varying input of ints, chars, floats, and strings
+ *   * @format: an array of chars signifying which data type to print
+ *    */
+void print_all(const char * const format, ...)
+{
+	char *separator = "";
+	int i, j = 0;
+	va_list valist;
+	datatype choice[] = { {'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL} };
+	/* iterate format; if datatype matched, access function via struct */
+	va_start(valist, format);
+	while (format != NULL && format[j] != '\0')
+	{
+		i = 0;
+		while (choice[i].letter != '\0')
+		{
+			if (choice[i].letter == format[j])
+			{
+				printf("%s", separator);
+				choice[i].func(valist); /*access va_arg later*/
+				separator = ", ";
+			}
+			i++;
+		}
+		j++;
+	}
+	va_end(valist);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,21 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
+/**
+ *struct dt- datatype
+ * @letter : character
+ * @func: function pointer
+ */
+typedef struct dt
+{
+	  char letter;
+	    void (*func)(va_list);
+} datatype;
+
+void print_c(va_list c);
+void print_n(va_list n);
+void print_s(va_list s);
+void print_fl(va_list f);
 #endif
