42fa6b723d3fba0aa4555403825cf523d3ae60e0
jmoseka <jamylam3@gmail.com>
2021-10-05 18:56:59 +0000

    prints all using variadic functions and checking specifiers

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,37 +2,39 @@
 #include<stdio.h>
 #include<stdarg.h>
 /**
- *  * print_char - prints char
- *   * @valist: valist
- *    */
+*print_char - prints char
+*@valist: valist
+*/
 void print_char(va_list valist)
 {
 	printf("%c", va_arg(valist, int));
 }
 /**
- *  * print_int - prints int
- *   * @valist: valist
- *    */
+*print_int - prints int
+*@valist: valist
+*/
 void print_int(va_list valist)
 {
 	printf("%d", va_arg(valist, int));
 }
 /**
- *  * print_float - prints float
- *   * @valist: valist
- *    */
+*print_float - prints float
+*@valist: valist
+*/
 void print_float(va_list valist)
 {
 	printf("%f", va_arg(valist, double));
 }
 /**
- *  * print_string - prints string
- *   * @valist: valist
- *    */
+*print_string - prints string
+*@valist: valist
+*/
 void print_string(va_list valist)
 {
 	char *s;
+
 	s = va_arg(valist, char *);
+
 	if (s == NULL)
 	{
 		printf("(nil)");
@@ -42,21 +44,23 @@ void print_string(va_list valist)
 }
 
 /**
- *  * print_all - print varying input of ints, chars, floats, and strings
- *   * @format: an array of chars signifying which data type to print
- *    */
+*print_all - print varying input of ints, chars, floats, and strings
+*@format: an array of chars signifying which data type to print
+*/
 void print_all(const char * const format, ...)
 {
 	char *separator = "";
 	int i, j = 0;
 	va_list valist;
+
 	datatype choice[] = { {'c', print_char},
 		{'i', print_int},
 		{'f', print_float},
 		{'s', print_string},
 		{'\0', NULL} };
-	/* iterate format; if datatype matched, access function via struct */
+	/*iterate format; if datatype matched, access function via struct */
 	va_start(valist, format);
+
 	while (format != NULL && format[j] != '\0')
 	{
 		i = 0;
