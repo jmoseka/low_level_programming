63e87137f594780b79bd91f5d38735a8867ae03a
jmoseka <jamylam3@gmail.com>
2021-10-08 12:40:26 +0000

    program that prints value and return no of nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,20 @@
 #include "lists.h"
 
 /**
- *print_list - prints all the elements of a list_t list
+ *print_list - prints all the elements in node list_o
  *@h: pointer
  *Return: number of nodes in the list
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t len = 0;
-	int i;
 	const list_t *c = h;
+	unsigned int i = 0;
+
 	for (i = 0; c; i++)
 	{
-		if (c->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", c->len, c->str);
+		printf("[%u] %s\n", c->len, c->str);
 		c = c->next;
-		len++;
 	}
-	return (len);
+	return (i);
 }
