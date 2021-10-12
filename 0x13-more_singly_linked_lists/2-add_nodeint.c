d0e30b7d01ce10bfd97d9e3f9bf9804acb90323d
jmoseka <jamylam3@gmail.com>
2021-10-12 16:09:40 +0000

    insert node in the beginning of list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *add_nodeint - insert node in the beginning
+ *@head: head pointer
+ *@n: integer value
+ *Return: a node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp = malloc(sizeof(n));
+
+	temp->n = n;
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stddef.h>
-
+#include <stdlib.h>
 /**
  *struct listint_s - singly linked list
  *@n: integer
@@ -19,5 +19,6 @@ typedef struct listint_s
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
