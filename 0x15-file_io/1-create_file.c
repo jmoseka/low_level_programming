12893d0643af0f4ca7f3d63aa5f18febb18bef35
jmoseka <jamylam3@gmail.com>
2021-10-18 17:45:11 +0000

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,7 @@
 size_t _strlen(char *str)
 {
 	size_t i;
+
 	for (i = 0; str[i]; i++)
 		;
 	return (i);
@@ -27,6 +28,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t len = 0;
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
