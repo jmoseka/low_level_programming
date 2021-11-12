9208ddd3a9655e6ab49a9641acf0e9a294884848
jmoseka <jamylam3@gmail.com>
2021-11-12 10:40:52 +0000

    a program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,63 @@
 #include "3-calc.h"
-#include <stddef.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- *get_op_func - cecks required operator and retrns necessary operator
- *
- *@s: operator string
- *Return: int
+ * op_add - add
+ * @a:integer
+ * @b:intger
+ * Return:result
  */
-
-int (*get_op_func(char *s)) (int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
-
-	for (i = 0; i < 5; i++)
-	{
-		if (*s == *ops[i].op)
-		{
-			return (ops[i].f);
-		}
-	}
-	return (NULL);
+return (a + b);
+}
+/**
+ * op_sub - sub
+ * @a:integer
+ * @b:intger
+ * Return:result
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - mul
+ * @a:integer
+ * @b:intger
+ * Return:result
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - div
+ * @a:integer
+ * @b:intger
+ * Return:result
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
+}
+/**
+ * op_mod - mod
+ * @a:integer
+ * @b:intger
+ * Return:result
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,33 +2,29 @@
 #include <stdlib.h>
 #include "3-calc.h"
 /**
- * main - performs airthmetic operations
- *@argc: arg count
- *@argv: arg value
- *Return:int
+ * main - performs simple operations
+ * @argc: arg count
+ * @argv: arg value
+ * Return:int
  */
 int main(int argc, char *argv[])
 {
 	int a, b, c;
-	char *s;
-
+	int (*fun)(int, int);
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	s = argv[2];
-
-	if ((get_op_func(s)) == NULL)
+	fun = get_op_func(argv[2]);
+	if (fun == NULL)
 	{
-		return (99);
+		printf("Error\n");
+		exit(99);
 	}
-
-	c = (get_op_func(s)) (a,b);
-
-	printf("%d", c);
-	
+	c = fun(a, b);
+	printf("%d\n", c);
 	return (0);
 }
