df9f63135a09206426737c907bae02b5546dc553
jmoseka <jamylam3@gmail.com>
2021-11-12 11:53:15 +0000

    a program that prints the opcodes of its own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
 {
 	int x, i;
 	unsigned char *p;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
@@ -24,7 +23,7 @@ int main(int argc, char *argv[])
 	}
 	p = (unsigned char *)main;
 	i = 0;
-	if (x > 0)
+	if(x > 0)
 	{
 		while (i < (x - 1))
 			printf("%02hhx ", p[i++]);
