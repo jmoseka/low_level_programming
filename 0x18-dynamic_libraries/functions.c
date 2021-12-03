58c93b4247dc802cbcf4a32e2822e5e93561a10d
jmoseka <jamylam3@gmail.com>
2021-12-03 20:56:31 +0000

    a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ *add - adds the two numbers
+ *Return: Always 0(Success)
+ */
+int add(void)
+{
+	int a;
+	int b;
+
+	return (a + b);
+}
+
+/**
+ *sub - subtracts two numbers
+ *Return: Always 0(Success)
+ */
+
+int sub(void)
+{
+	int a;
+	int b;
+
+	return (a - b);
+}
+
+/**
+ *mul - multiplication of two numbers
+ *Return: Always 0(Success)
+ */
+
+int mul(void)
+{
+	int a;
+	int b;
+
+	return (a * b);
+}
+
+/**
+ *division - division of two numbers
+ *Return: Always 0(Success)
+ */
+
+int division(void)
+{
+	int a;
+	int b;
+
+	return (a / b);
+}
+
+/**
+ *modulo - modulo of two numbers
+ *Return: Always 0(Success)
+ */
+
+int modulo(void)
+{
+	int a;
+	int b;
+
+	return (a % b);
+
+}
